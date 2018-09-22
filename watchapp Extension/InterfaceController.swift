50ed17e7d244184b1ba734995edbcfab2854e7b6
Joakim Stien <joakim.stien@gmail.com>
2018-09-22 10:21:51 +0200

    Proper icon creation, added empty watch app

---
+++ watchapp Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watchapp Extension
+//
+//  Created by pimms on 22/09/2018.
+//  Copyright © 2018 pimms. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
